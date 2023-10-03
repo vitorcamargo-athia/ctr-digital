a1a72a97cfeea4fbb04001ed5b98a2ac70b9fc5c
Vitor Camargo <vitorcamargo@ATHIA.LOCAL>
2023-10-03 10:46:56 -0300

    direcionado para produção

--- pages/assinatura.vue
+++ pages/assinatura.vue
@@ -54,14 +54,13 @@ export default defineComponent({
     },
     mounted() {
         // if (window.location.hostname == "localhost") {
-        //this.token = 'd796f29c-0a7b-448c-a8f7-aeb476ba5c41';
-        this.token = '6830ce18-ee25-4861-b337-c1db7fe79df0';
-        this.url = this.urlHomolog;
-        /*  } else {
-              this.token = 'd796f29c-0a7b-448c-a8f7-aeb476ba5c41';
+        //this.token = '6830ce18-ee25-4861-b337-c1db7fe79df0';
+        //this.url = this.urlHomolog;
+        //}else {
+              this.token = 'a7063c4f-52bb-4f53-a5a4-5e6733e337f5';
               this.url = this.urlProd;
-          }
-  */
+  //        }
+  
         let uri = window.location.search.substring(1);
         let params = new URLSearchParams(uri);
         this.contrato = params.get("contrato");
--- pages/print.vue
+++ pages/print.vue
@@ -66,6 +66,7 @@ export default defineComponent({
         this.token = '6830ce18-ee25-4861-b337-c1db7fe79df0';
         /* } else {
              this.url = this.urlProd;
+             this.token = 'a7063c4f-52bb-4f53-a5a4-5e6733e337f5'
          }*/
 
         //this.getData();
